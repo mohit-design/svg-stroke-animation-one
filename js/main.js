df37290b74c12f4d0428566b2dd44a4801644da8
Mohit Patel <3443mohit@gmail.com>
2020-04-23 18:57:14 -0700

    add js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Disabled Ctrl+U and Ctrl+C
+$(document).keydown(function(event) { 
+	var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+	if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
+	  //disable key press porcessing
+	  return false; 
+	}
+});
+// Disbaled Right Click
+window.oncontextmenu = function () {
+  return false;
+}
+$(document).keydown(function (event) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+    return false;
+  }
+});
